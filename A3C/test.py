6fc663b4f8e476ec9d22b5c6e583c75346e28bbf
m-star18 <ryuuchei@ezweb.ne.jp>
2020-09-15 02:45:51 +0900

    Add get_args

--- A3C/test.py
+++ A3C/test.py
@@ -9,3 +9,15 @@ import torch.nn.functional as F
 
 from src.env import create_train_env
 from src.model import ActorCritic
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--world", type=int, default=1)
+    parser.add_argument("--stage", type=int, default=1)
+    parser.add_argument("--action_type", type=str, default="complex")
+    parser.add_argument("--saved_path", type=str, default="../trained_models")
+    parser.add_argument("--output_path", type=str, default="output")
+    args = parser.parse_args()
+
+    return args
