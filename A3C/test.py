1fcb27c52db34c96427b045987431d1d2c6d7e23
m-star18 <ryuuchei@ezweb.ne.jp>
2020-09-15 02:46:06 +0900

    Add test

--- A3C/test.py
+++ A3C/test.py
@@ -21,3 +21,42 @@ def get_args():
     args = parser.parse_args()
 
     return args
+
+
+def test(opt):
+    torch.manual_seed(123)
+    env, num_states, num_actions = create_train_env(opt.world, opt.stage, opt.action_type,
+                                                    f"{opt.output_path}/video_{opt.world}_{opt.stage}.mp4")
+    model = ActorCritic(num_states, num_actions)
+
+    model.load_state_dict(torch.load(f"{opt.saved_path}/a3c_super_mario_bros_{opt.world}_{opt.stage}",
+                                     map_location=lambda storage, loc: storage))
+    model.eval()
+    state = torch.from_numpy(env.reset())
+    done = True
+
+    while True:
+        if done:
+            h_0 = torch.zeros((1, 512), dtype=torch.float)
+            c_0 = torch.zeros((1, 512), dtype=torch.float)
+            env.reset()
+        else:
+            h_0 = h_0.detach()
+            c_0 = c_0.detach()
+
+        logits, value, h_0, c_0 = model(state, h_0, c_0)
+        policy = F.softmax(logits, dim=1)
+        action = torch.argmax(policy).item()
+        action = int(action)
+        state, reward, done, info = env.step(action)
+        state = torch.from_numpy(state)
+        env.render()
+
+        if info["flag_get"]:
+            print(f"World {opt.world} stage {opt.stage} completed")
+            break
+
+
+if __name__ == "__main__":
+    opt = get_args()
+    test(opt)
