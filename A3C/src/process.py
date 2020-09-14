6c02e7d1d717a27207cb41633ce417c20dc3aac2
m-star18 <ryuuchei@ezweb.ne.jp>
2020-09-15 02:43:27 +0900

    Add test_process

--- A3C/src/process.py
+++ A3C/src/process.py
@@ -20,6 +20,36 @@ def local_test(index, opt, global_model):
     curr_step = 0
     actions = deque(maxlen=opt.max_actions)
 
+    while True:
+        curr_step += 1
+        if done:
+            local_model.load_state_dict(global_model.state_dict())
+
+        with torch.no_grad():
+            if done:
+                h_0 = torch.zeros((1, 512), dtype=torch.float)
+                c_0 = torch.zeros((1, 512), dtype=torch.float)
+            else:
+                h_0 = h_0.detach()
+                c_0 = c_0.detach()
+
+        logits, value, h_0, c_0 = local_model(state, h_0, c_0)
+        policy = F.softmax(logits, dim=1)
+        action = torch.argmax(policy).item()
+        state, reward, done, _ = env.step(action)
+        env.render()
+        actions.append(action)
+
+        if curr_step > opt.num_global_steps or actions.count(actions[0]) == actions.maxlen:
+            done = True
+
+        if done:
+            curr_step = 0
+            actions.clear()
+            state = env.reset()
+
+        state = torch.from_numpy(state)
+
 
 def local_train(index, opt, global_model, optimizer, save=False):
     torch.manual_seed(123 + index)
