b07db20debc125940a8e682ed64bb143a3075f62
m-star18 <ryuuchei@ezweb.ne.jp>
2020-09-15 02:02:34 +0900

    Add process_frame

--- A3C/src/env.py
+++ A3C/src/env.py
@@ -22,3 +22,13 @@ class Monitor:
 
     def record(self, image_array):
         self.pipe.stdin.write(image_array.tostring())
+
+
+def process_frame(frame):
+    if frame is not None:
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        frame = cv2.resize(frame, (84, 84))[None, :, :] / 255.
+        return frame
+
+    else:
+        return np.zeros((1, 84, 84))
