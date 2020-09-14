169073fc161be63c77cc7e357262789e6caddef4
m-star18 <ryuuchei@ezweb.ne.jp>
2020-09-15 02:37:46 +0900

    Add local_train

--- A3C/src/process.py
+++ A3C/src/process.py
@@ -8,3 +8,18 @@ from torch.distributions import Categorical
 
 from src.env import create_train_env
 from src.model import ActorCritic
+
+
+def local_train(index, opt, global_model, optimizer, save=False):
+    torch.manual_seed(123 + index)
+    if save:
+        start_time = timeit.default_timer()
+
+    writer = SummaryWriter(opt.log_path)
+    env, num_states, num_actions = create_train_env(opt.world, opt.stage, opt.action_type)
+    local_model = ActorCritic(num_states, num_actions)
+    local_model.train()
+    state = torch.from_numpy(env.reset())
+    done = True
+    curr_step = 0
+    curr_episode = 0
