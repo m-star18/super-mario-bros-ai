6cf98ec0aaf6141f03c6a3a08a594594ba2ce762
m-star18 <ryuuchei@ezweb.ne.jp>
2020-09-15 02:01:46 +0900

    Add Monitor

--- A3C/src/env.py
+++ A3C/src/env.py
@@ -8,3 +8,17 @@ from gym.spaces import Box
 from gym_super_mario_bros.actions import (COMPLEX_MOVEMENT, RIGHT_ONLY,
                                           SIMPLE_MOVEMENT)
 from nes_py.wrappers import JoypadSpace
+
+
+class Monitor:
+    def __init__(self, width, height, saved_path):
+
+        self.command = ["ffmpeg", "-y", "-f", "rawvideo", "-vcodec", "rawvideo", "-s", f"{width}X{height}",
+                        "-pix_fmt", "rgb24", "-r", "80", "-i", "-", "-an", "-vcodec", "mpeg4", saved_path]
+        try:
+            self.pipe = sp.Popen(self.command, stdin=sp.PIPE, stderr=sp.PIPE)
+        except FileNotFoundError:
+            pass
+
+    def record(self, image_array):
+        self.pipe.stdin.write(image_array.tostring())
