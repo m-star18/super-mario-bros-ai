7d9efe8c92877c13360631c91aad932d7cbdf585
m-star18 <31807@toyota.kosen-ac.jp>
2021-06-10 10:26:48 +0900

    Update constants

--- A3C/train.py
+++ A3C/train.py
@@ -12,22 +12,35 @@ from src.env import create_train_env
 from src.model import ActorCritic
 from src.optimizer import GlobalAdam
 from src.process import local_test, local_train
+from src.const import (
+    ACTION_TYPE,
+    LR,
+    GAMMA,
+    TAU,
+    BETA,
+    NUM_LOCAL_STEP,
+    NUM_GLOBAL_STEP,
+    NUM_PROCESS,
+    SAVE_INTERVAL,
+    MAX_ACTIONS,
+    SEED,
+)
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--world", type=int, default=1)
     parser.add_argument("--stage", type=int, default=1)
-    parser.add_argument("--action_type", type=str, default="complex")
-    parser.add_argument('--lr', type=float, default=1e-4)
-    parser.add_argument('--gamma', type=float, default=0.9, help='discount factor for rewards')
-    parser.add_argument('--tau', type=float, default=1.0, help='parameter for GAE')
-    parser.add_argument('--beta', type=float, default=0.01, help='entropy coefficient')
-    parser.add_argument("--num_local_steps", type=int, default=50)
-    parser.add_argument("--num_global_steps", type=int, default=5e6)
-    parser.add_argument("--num_processes", type=int, default=6)
-    parser.add_argument("--save_interval", type=int, default=500, help="Number of steps between savings")
-    parser.add_argument("--max_actions", type=int, default=200, help="Maximum repetition steps in test phase")
+    parser.add_argument("--action_type", type=str, default=ACTION_TYPE)
+    parser.add_argument('--lr', type=float, default=LR)
+    parser.add_argument('--gamma', type=float, default=GAMMA, help='discount factor for rewards')
+    parser.add_argument('--tau', type=float, default=TAU, help='parameter for GAE')
+    parser.add_argument('--beta', type=float, default=BETA, help='entropy coefficient')
+    parser.add_argument("--num_local_steps", type=int, default=NUM_LOCAL_STEP)
+    parser.add_argument("--num_global_steps", type=int, default=NUM_GLOBAL_STEP)
+    parser.add_argument("--num_processes", type=int, default=NUM_PROCESS)
+    parser.add_argument("--save_interval", type=int, default=SAVE_INTERVAL, help="Number of steps between savings")
+    parser.add_argument("--max_actions", type=int, default=MAX_ACTIONS, help="Maximum repetition steps in test phase")
     parser.add_argument("--log_path", type=str, default="tensorboard/a3c_super_mario_bros")
     parser.add_argument("--saved_path", type=str, default="../trained_models")
     parser.add_argument("--load_from_previous_stage", type=bool, default=False,
@@ -38,7 +51,7 @@ def get_args():
 
 
 def train(opt):
-    torch.manual_seed(123)
+    torch.manual_seed(SEED)
 
     if os.path.isdir(opt.log_path):
         shutil.rmtree(opt.log_path)
